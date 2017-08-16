0eed5c28f0ac991ac7d2f5fa1c1fa579c58f4957
cjbarnaby <cjbarnaby@gmail.com>
2017-08-16 10:28:56 +1000

    consistent bubble styling, simplified response types

--- js/app.js
+++ js/app.js
@@ -4,23 +4,33 @@ var chatbot = {
   enterKeyCode: 13,
   loading: false,
   responseTypes: {
-    0: "default", // default intents. TODO: Confirm (fulfilment.messages.type === 0) indicates default
+    0: "default",
+    4: "custom"
+  },
+  messageTypes: {
     10: "text",
     11: "card",
     12: "quick"
   },
-  accessToken: "b745f3f6e65b458e895add17566b55dc"
+  accessToken: "b745f3f6e65b458e895add17566b55dc",
+  get client() {
+    return new ApiAi.ApiAiClient({accessToken: this.accessToken});
+  },
+  get sessionId() {
+    return this.client.sessionId;
+  },
+  sendText: function(text, options) {
+    if (options === undefined) {
+      // simple text query, no options to include in the request (eg context, etc)
+      return this.client.textRequest(text);
+    } else {
+      // complex request, including options (eg context, etc)
+    }
+  }, init: function() {
+
+  }
 };
 
-
-/**************************/
-/******** ELEMENTS ********/
-/**************************/
-// var $input;
-// var $chatbotWindow;
-// var $loading;
-
-chatbot.client = new ApiAi.ApiAiClient({accessToken: chatbot.accessToken});
 
 chatbot.toggleLoading = function() {
   if (!chatbot.loading) {
@@ -31,27 +41,9 @@ chatbot.toggleLoading = function() {
   chatbot.loading = !chatbot.loading;
 };
 
-chatbot.sendText = function(text) {
-  return chatbot.client.textRequest(text);
-};
-
-chatbot.handleQuery = function(input) {
-  chatbot.toggleLoading();
-  chatbot.sendText(input).then(function(response) {
-    chatbot.attachResponse(response);
-  }).catch(function(err) {
-    console.log(err);
-    chatbot.attachResponse(err, "error");
-  });
-};
-
-chatbot.handleInput = function(query) {
-  chatbot.attachQuery(query);
-  chatbot.handleQuery(query);
-};
-
 chatbot.sendClicked = function() {
   var value = getInputValue();
+  $input.val("");
   chatbot.handleInput(value);
 };
 
@@ -73,6 +65,7 @@ chatbot.inputKeyDown = function(event) {
     return;
   }
   var value = chatbot.getInputValue();
+  $input.val("");
   chatbot.handleInput(value);
 };
 
@@ -89,6 +82,11 @@ chatbot.inputKeyDown = function(event) {
 //
 // };
 
+chatbot.handleInput = function(query) {
+  chatbot.attachQuery(query);
+  chatbot.handleQuery(query);
+};
+
 chatbot.attachQuery = function(query) {
   // TODO: Refactor — basically identical to text response
   query = query.split("\n");
@@ -104,9 +102,18 @@ chatbot.attachQuery = function(query) {
   $query.append($bubble);
   $chatbotWindow.append($query);
   $chatbotWindow.animate({ scrollTop: $chatbotWindow[0].scrollHeight}, 500);
-  $input.val("");
+
 };
 
+chatbot.handleQuery = function(input) {
+  chatbot.toggleLoading();
+  chatbot.sendText(input).then(function(response) {
+    chatbot.attachResponse(response);
+  }).catch(function(err) {
+    console.log(err);
+    chatbot.attachResponse(err, "error");
+  });
+};
 
 chatbot.attachResponse = function(response, error) {
   var $response;
@@ -120,30 +127,39 @@ chatbot.attachResponse = function(response, error) {
   var $cardButton;
   var $quickReply;
   var $text;
+  var responseType;
+  var messageType;
   var payload;
   var title;
   var replies;
 
-
   // TODO: Refactor - all responses are similar
   if (error !== undefined) {
+    console.log("An error has occurred. Response below:");
     console.log(response);
-    console.log("An error has occurred.");
   // TODO: Add error handling
     return;
   }
 
   $response = $("<div class='response'></div>");
 
-  try {
+  // try {
+  //   payload = response.result.fulfillment.messages[0].payload;
+  // } catch(e) {
+  //   payload = null;
+  // }
+
+  responseType = chatbot.responseTypes[response.result.fulfillment.messages[0].type];
+
+  if (responseType !== "default") {
     payload = response.result.fulfillment.messages[0].payload;
-  } catch(e) {
+    messageType = chatbot.messageTypes[payload.type];
+  } else {
     payload = null;
+    messageType = chatbot.messageTypes[10]; // This assumes that all default/type 0 responses from the API will be basic text messages.
   }
 
-  var responseType = payload ? chatbot.responseTypes[payload.type] : chatbot.responseTypes[0];
-
-  if ((responseType === "text") || (responseType === "default")) {
+  if (messageType === "text") {
     text = payload ? payload.text.split("\n") : new Array(response.result.fulfillment.messages[0].speech);
     $bubble = $("<div class='bubble'></div>");
     $bubbleText = $("<div class='bubble__text'>");
@@ -157,8 +173,7 @@ chatbot.attachResponse = function(response, error) {
     $bubble.append($bubbleText);
     $response.append($bubble);
 
-
-  } else if (responseType === "quick") {
+  } else if (messageType === "quick") {
     title = payload.title;
     replies = payload.replies;
     $bubble = $("<div class='bubble'></div>");
@@ -180,7 +195,7 @@ chatbot.attachResponse = function(response, error) {
     $response.append($bubble);
     $response.append($quickReply);
 
-  } else if (responseType === "card") {
+  } else if (messageType === "card") {
     $card = $("<div class='card'></div>");
     $cardImage = $("<div class='card__image'></div>");
     $cardTitle = $("<div class='card__title'></div>");
@@ -226,7 +241,7 @@ chatbot.attachResponse = function(response, error) {
 $(document).ready(function() {
 
   /**************************/
-  /******** ELEMENTS ********/
+  /***** jQUERY OBJECTS *****/
   /**************************/
   $input = $(".input__text");
   $chatbotWindow = $(".chatbot__window");
@@ -239,11 +254,12 @@ $(document).ready(function() {
   $send.on("click", chatbot.sendClicked);
   $input.on("keydown", function(e) {
     if (chatbot.getInputValue() === "") {
-      return; //API throws an error to blank queries.
+      return; //API throws an error to blank queries, so just return for the timebeing.
     }
     chatbot.inputKeyDown(e);
   });
   $(document).on("click", ".quick-reply__button, .card__button", function(e) {
     chatbot.buttonClicked(e);
   });
+
 });
